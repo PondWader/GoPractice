1c3a2bf76dc79615a7ca71d04c7f66cc43980b6d
PondWader <66561610+PondWader@users.noreply.github.com>
2023-09-07 20:33:49 +0100

    Calculate block place location with face

--- server/context/build_handlers.go
+++ server/context/build_handlers.go
@@ -5,12 +5,29 @@ import (
 	"github.com/PondWader/GoPractice/server/world"
 )
 
+func getBlockLocation(pos *packets.Position, face int8) (x int32, y int32, z int32) {
+	x, y, z = pos.X, pos.Y, pos.Z
+	switch face {
+	case 0:
+		y -= 1
+	case 1:
+		y += 1
+	case 2:
+		z -= 1
+	case 3:
+		z += 1
+	case 4:
+		x -= 1
+	case 5:
+		x += 1
+	}
+	return
+}
+
 func (p *ContextPlayer) handleBlockPlace(packet interface{}) {
 	placePacket := packet.(*packets.SPlayerBlockPlacement)
 
-	x := placePacket.Location.X
-	y := placePacket.Location.Y
-	z := placePacket.Location.Z
+	x, y, z := getBlockLocation(placePacket.Location, placePacket.Face)
 
 	if y >= 0 && y <= 255 {
 		xInChunk, yInChunk, zInChunk := world.CoordsInChunk(int(x&0xf), int(y&0xf), int(z&0xf))
